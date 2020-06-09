f15c4188ecc20b4aae46d9a7c295664ed40c1055
Yuhao's Macbook <yuhao.zhong@code.berlin>
2020-06-09 17:07:29 +0200

    Place Cube or Sphere on the plane based on tap action

---
+++ Assets/Scripts/PlacementController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+//[RequireComponent(typeof(ARRaycastManager))]
+
+public class PlacementController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject cubePrefab;
+    [SerializeField] private GameObject spherePrefab;
+
+    private ARRaycastManager raycastManager;
+    private ARPlaneManager arPlaneManager;
+    private Camera arCam;
+
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    void Awake()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+        arPlaneManager = GetComponent<ARPlaneManager>();
+
+    }
+
+
+    void Start()
+    {
+        arCam = GetComponentInChildren<Camera>();
+    }
+
+    void Update()
+    {
+
+        // If detected QR Code 1
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = arCam.ScreenPointToRay(Input.mousePosition);
+            if (raycastManager.Raycast(ray, hits))
+            {
+                Pose pose = hits[0].pose;
+                Instantiate(spherePrefab, pose.position, pose.rotation);
+            }
+        }
+
+        // If detected QR Code 2
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = arCam.ScreenPointToRay(Input.mousePosition);
+            if (raycastManager.Raycast(ray, hits))
+            {
+                Pose pose = hits[0].pose;
+                Instantiate(cubePrefab, pose.position, pose.rotation);
+            }
+        }
+
+
+    }
+
+}
